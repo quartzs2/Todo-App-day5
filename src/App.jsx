731755b48bb943f31ef651396fa1e0ddbd831abd
quartzs2 <csh001231@gmail.com>
2025-04-08 10:51:02 +0900

    feat: 5일차 과제

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import "./App.css";
 
 function App() {
   const [todoList, setTodoList] = useState([
-    { id: 0, content: "123" },
-    { id: 1, content: "코딩 공부하기" },
-    { id: 2, content: "잠 자기" },
+    { id: 0, content: "123", isDone: false },
+    { id: 1, content: "코딩 공부하기", isDone: true },
+    { id: 2, content: "잠 자기", isDone: false },
   ]);
 
   return (
-    <>
+    <main>
+      <h1>Todo List</h1>
+      <hr />
       <TodoList todoList={todoList} setTodoList={setTodoList} />
       <hr />
       <TodoInput todoList={todoList} setTodoList={setTodoList} />
-    </>
+    </main>
   );
 }
 
@@ -22,13 +24,10 @@ function TodoInput({ todoList, setTodoList }) {
 
   return (
     <>
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
+      <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
       <button
         onClick={() => {
-          const newTodo = { id: Number(new Date()), content: inputValue };
+          const newTodo = { id: Number(new Date()), content: inputValue, isDone: false };
           const newTodoList = [...todoList, newTodo];
           setTodoList(newTodoList);
           setInputValue("");
@@ -52,33 +51,56 @@ function TodoList({ todoList, setTodoList }) {
 
 function Todo({ todo, setTodoList }) {
   const [inputValue, setInputValue] = useState("");
+  const [isModify, setIsModify] = useState(false);
+
   return (
     <li>
-      {todo.content}
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          setTodoList((prev) =>
-            prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
+      {!isModify && (
+        <span
+          onClick={() =>
+            setTodoList((prevTodos) =>
+              prevTodos.map((data) => {
+                if (todo.id === data.id) {
+                  return { ...todo, isDone: !data.isDone };
+                }
+                return data;
+              })
             )
-          );
-        }}
-      >
-        수정
-      </button>
-      <button
-        onClick={() => {
-          setTodoList((prev) => {
-            return prev.filter((el) => el.id !== todo.id);
-          });
-        }}
-      >
-        삭제
-      </button>
+          }
+          className={todo.isDone ? "done" : undefined}
+        >
+          {todo.content}
+        </span>
+      )}
+      {isModify && (
+        <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
+      )}
+
+      <div className="buttons">
+        <button
+          onClick={() => {
+            if (isModify) {
+              setTodoList((prev) =>
+                prev.map((el) => (el.id === todo.id ? { ...el, content: inputValue } : el))
+              );
+              setIsModify(false);
+            } else {
+              setIsModify(true);
+            }
+          }}
+        >
+          {isModify ? "완료" : "수정"}
+        </button>
+        <button
+          onClick={() => {
+            setTodoList((prev) => {
+              return prev.filter((el) => el.id !== todo.id);
+            });
+          }}
+        >
+          삭제
+        </button>
+      </div>
     </li>
   );
 }
